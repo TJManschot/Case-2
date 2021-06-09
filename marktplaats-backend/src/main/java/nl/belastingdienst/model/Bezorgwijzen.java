b8eb6db30375712cc2e35674150814d88281af91
TJManschot <tj.manschot@belastingdienst.nl>
2021-06-09 09:29:28 +0200

    first commit

---
+++ marktplaats-backend/src/main/java/nl/belastingdienst/model/Bezorgwijzen.java
@@ -0,0 +1,19 @@
+package nl.belastingdienst.model;
+
+public enum Bezorgwijzen {
+    MAGAZIJN(false),
+    OPHALEN(true),
+    VERSTUREN(false),
+    VERSTUREN_ONDER_REMBOURS(false);
+
+
+
+    Bezorgwijzen(boolean verplichtAdres) {
+        this.verplichtAdres = verplichtAdres;
+    }
+    private final boolean verplichtAdres;
+
+    public boolean getVerplichtAdres() {
+        return verplichtAdres;
+    }
+}
