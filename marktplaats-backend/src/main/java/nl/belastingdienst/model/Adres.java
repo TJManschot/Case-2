a0bccf81b159f20fd7f60a4059837cc715b2c85f
TJManschot <tj.manschot@belastingdienst.nl>
2021-06-09 11:39:00 +0200

    Getters en setters voor fields in Adres.java

--- marktplaats-backend/src/main/java/nl/belastingdienst/model/Adres.java
+++ marktplaats-backend/src/main/java/nl/belastingdienst/model/Adres.java
@@ -4,8 +4,40 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 public class Adres {
-    String straat;
-    String huisnummer;
-    String postcode;
-    String stad;
+    private String straat;
+    private String huisnummer;
+    private String postcode;
+    private String stad;
+
+    public String getStraat() {
+        return straat;
+    }
+
+    public void setStraat(String straat) {
+        this.straat = straat;
+    }
+
+    public String getHuisnummer() {
+        return huisnummer;
+    }
+
+    public void setHuisnummer(String huisnummer) {
+        this.huisnummer = huisnummer;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getStad() {
+        return stad;
+    }
+
+    public void setStad(String stad) {
+        this.stad = stad;
+    }
 }
