15fe3847b82d5b8b95cabecc3cdaced834f6ece5
TJManschot <tj.manschot@belastingdienst.nl>
2021-06-09 12:56:51 +0200

    Pad aangepast naar /api/, en pom aangevuld met de nodige properties

--- marktplaats-backend/src/main/java/nl/belastingdienst/App.java
+++ marktplaats-backend/src/main/java/nl/belastingdienst/App.java
@@ -3,6 +3,6 @@ package nl.belastingdienst;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("")
+@ApplicationPath("api")
 public class App extends Application {
 }
