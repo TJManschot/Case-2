1ced5612bd78eed6601de71fb05f4c9da507b2a5
TJManschot <tj.manschot@belastingdienst.nl>
2021-06-10 11:25:33 +0200

    Back-end stuurt response, front-end vangt deze af.

--- marktplaats-backend/src/main/java/nl/belastingdienst/resources/GebruikersResource.java
+++ marktplaats-backend/src/main/java/nl/belastingdienst/resources/GebruikersResource.java
@@ -7,6 +7,8 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("gebruikers")
@@ -27,14 +29,31 @@ public class GebruikersResource implements JsonResource {
     }
 
     @POST @Path("login")
-    public void login(Gebruiker input) {
+    public Response login(Gebruiker input) {
         String gebruikersnaam = input.getGebruikersnaam();
         String wachtwoord = input.getWachtwoord();
 
-        System.out.println(gebruikersnaam + " " + wachtwoord);
-        List<Gebruiker> resultaat = gebruikerDao.getByGebruikersnaam(gebruikersnaam);
-        if (resultaat.size() == 1) {
-            System.out.println(resultaat.get(0).getWachtwoord());
+        List<Gebruiker> gebruikerList = gebruikerDao.getByGebruikersnaam(gebruikersnaam);
+
+        if(gebruikerList.size() == 0) {
+            return Response.status(401)
+                    .type(MediaType.TEXT_PLAIN_TYPE)
+                    .entity("De opgegeven gebruikersnaam werd niet gevonden.")
+                    .build();
         }
+
+        Gebruiker gebruiker = gebruikerList.get(0);
+
+        if(!wachtwoord.equals(gebruiker.getWachtwoord())) {
+            return Response.status(401)
+                    .type(MediaType.TEXT_PLAIN_TYPE)
+                    .entity("Het opgegeven wachtwoord is niet correct.")
+                    .build();
+        }
+
+        return Response.status(200)
+                .type(MediaType.APPLICATION_JSON_TYPE)
+                .entity(gebruiker)
+                .build();
     }
 }
